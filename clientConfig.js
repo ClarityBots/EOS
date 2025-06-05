90635b1ddad80180c6792385cbedaf752caf6ab0
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-05 05:49:39 -0600

    Moved client config to js

---
+++ clientConfig.js
@@ -0,0 +1,38 @@
+// clientConfig.js
+export const clients = {
+  alder: {
+    heading: "Alder Construction's",
+    background: "images/alder_image.jpg",
+    logo: "images/alder_logo.png",
+  },
+  business_intuition: {
+    heading: "Business Intuition's",
+    background: "images/business_intuition_image.jpg",
+    logo: "images/business_intuition_logo.svg",
+  },
+  cope: {
+    heading: "COP Construction's",
+    background: "images/cop_image.jpg",
+    logo: "images/cop_logo.png",
+  },
+  remedyww: {
+    heading: "Remedy World Wide's",
+    background: "images/remedyww_image.jpg",
+    logo: "images/remedyww_logo.png",
+  },
+  skyline: {
+    heading: "Skyline Electric's",
+    background: "images/skyline_image.png",
+    logo: "images/skyline_logo.png",
+  },
+  vlcm: {
+    heading: "VLCM's",
+    background: "images/vlcm_image.jpg",
+    logo: "images/vlcm_logo.svg",
+  },
+  winward: {
+    heading: "Winward Electric's",
+    background: "images/winward_image.jpg",
+    logo: "images/winward_logo.png",
+  },
+};
