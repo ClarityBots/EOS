d335bb85c8da42a031c1b2289195f975b18320d2
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-05 06:29:24 -0600

    Cleaned up script section

--- clientConfig.js
+++ clientConfig.js
@@ -1,38 +1,70 @@
 // clientConfig.js
+
 export const clients = {
   alder: {
     heading: "Alder Construction's",
     background: "images/alder_image.jpg",
     logo: "images/alder_logo.png",
+    altText: "Alder Construction Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
   business_intuition: {
     heading: "Business Intuition's",
     background: "images/business_intuition_image.jpg",
     logo: "images/business_intuition_logo.svg",
+    altText: "Business Intuition Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
-  cope: {
+  cop: {
     heading: "COP Construction's",
     background: "images/cop_image.jpg",
     logo: "images/cop_logo.png",
+    altText: "COP Construction Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
   remedyww: {
     heading: "Remedy World Wide's",
     background: "images/remedyww_image.jpg",
     logo: "images/remedyww_logo.png",
+    altText: "Remedy World Wide Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
   skyline: {
     heading: "Skyline Electric's",
     background: "images/skyline_image.png",
     logo: "images/skyline_logo.png",
+    altText: "Skyline Electric Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
   vlcm: {
     heading: "VLCM's",
     background: "images/vlcm_image.jpg",
     logo: "images/vlcm_logo.svg",
+    altText: "VLCM Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
   winward: {
     heading: "Winward Electric's",
     background: "images/winward_image.jpg",
     logo: "images/winward_logo.png",
+    altText: "Winward Electric Logo",
+    preloadImage: true,
+    basePath: "images/"
   },
+
+  // 🌐 Default fallback for unknown subdomains
+  default: {
+    heading: "ClarityBots",
+    background: "images/default_background.jpg",
+    logo: "images/logo-default.svg",
+    altText: "ClarityBots Default Logo",
+    preloadImage: false,
+    basePath: "images/"
+  }
 };
