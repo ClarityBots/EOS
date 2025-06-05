e795e8e7769173ef30b1cb75ba0da1a43c492ddc
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-05 09:07:11 -0600

    First ChatGPT pass on clientConfig.js

---
+++ clientConfig.js
@@ -0,0 +1,8 @@
+export const clients = {
+  business_intuition: {
+    background: "images/business_intuition_image.jpg",
+    heading: "Tools to Build Vision and Traction™",
+    logo: "images/logo-default.svg"
+  },
+  // Add more subdomains if needed
+};
