a0c55c88e1cdfdc3e0ae3802a990c9cfc4ee2197
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-05 08:42:55 -0600

    Rolled back index.HTML from Google drive

---
+++ clientConfig.js
@@ -0,0 +1,78 @@
+// clientConfig.js
+
+export const clients = {
+  alder: {
+    heading: "Alder Construction's",
+    background: "images/alder_image.jpg",
+    logo: "images/alder_logo.png",
+    altText: "Alder Construction Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#445777" // Alder's primary brand color
+  },
+  business_intuition: {
+    heading: "Business Intuition's",
+    background: "images/business_intuition_image.jpg",
+    logo: "images/business_intuition_logo.svg",
+    altText: "Business Intuition Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#F04E23" // EOS red-orange
+  },
+  cop: {
+    heading: "COP Construction's",
+    background: "images/cop_image.jpg",
+    logo: "images/cop_logo.png",
+    altText: "COP Construction Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#6C7C90" // COP's primary brand color
+  },
+  remedyww: {
+    heading: "Remedy World Wide's",
+    background: "images/remedyww_image.jpg",
+    logo: "images/remedyww_logo.png",
+    altText: "Remedy World Wide Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#A9B7C6" // Remedy World Wide's brand color
+  },
+  skyline: {
+    heading: "Skyline Electric's",
+    background: "images/skyline_image.png",
+    logo: "images/skyline_logo.png",
+    altText: "Skyline Electric Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#FBBF24" // Skyline Electric's brand color
+  },
+  vlcm: {
+    heading: "VLCM's",
+    background: "images/vlcm_image.jpg",
+    logo: "images/vlcm_logo.svg",
+    altText: "VLCM Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#28A745" // VLCM's primary brand color
+  },
+  winward: {
+    heading: "Winward Electric's",
+    background: "images/winward_image.jpg",
+    logo: "images/winward_logo.png",
+    altText: "Winward Electric Logo",
+    preloadImage: true,
+    basePath: "images/",
+    brandColor: "#9C27B0" // Winward Electric's brand color
+  },
+
+  // 🌐 Default fallback for unknown subdomains
+  default: {
+    heading: "ClarityBots",
+    background: "images/default_background.jpg",
+    logo: "images/logo-default.svg",
+    altText: "ClarityBots Default Logo",
+    preloadImage: false,
+    basePath: "images/",
+    brandColor: "#FF7900" // Default brand color (EOS orange)
+  }
+};
