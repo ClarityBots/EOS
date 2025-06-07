8eefa7c53bd64eb22b5f5f1796b45faa05bd0af6
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-07 15:03:09 -0600

    Fixed EOS path

--- clientConfig.js
+++ clientConfig.js
@@ -28,7 +28,7 @@ export const clients = {
     basePath: "images/",
     brandColor: "#6C7C90",
   },
-  cop: {
+  eosww: {
     heading: "EOS Worldwide's",
     background: "images/eosww_image.jpg",
     logo: "images/eosww_logo.webp",
