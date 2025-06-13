3c123eae07e14a024d85cf36969e163d3e10eb55
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-13 17:17:25 -0600

    KF: Version 1.5 and a prayer

--- clientConfig.js
+++ clientConfig.js
@@ -3,7 +3,7 @@
 export const clients = {
   alder: {
     heading: "Alder Construction's",
-    background: "images/alder_background.jpg",
+    background: "https://source.unsplash.com/1920x1080/?construction",
     logo: "images/alder_logo.png",
     altText: "Alder Construction Logo",
     preloadImage: true,
