b1b84d6f5e0af3105f24d70b835d0b0b2af4ecb2
ClarityBots <Kyle@BusinessIntuition.com>
2025-06-14 20:12:17 -0600

    Add testclient config and assets

---
+++ public/js/clientConfig.js
@@ -0,0 +1,14 @@
+// clientConfig.js
+
+export const clients = {
+	testclient: {
+		heading: "Test Clients's",
+		background: "images/testclient/background.jpg",
+		logo: "images/testclient/logo.svg",
+		altText: "Test Client Logo",
+		preloadImage: true,
+		basePath: "images/",
+		brandColor: "#142233",
+		tools: ["rocks", "vision", "scorecard"]
+	}
+};
